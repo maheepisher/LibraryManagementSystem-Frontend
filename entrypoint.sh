882a67379559766310e46121e4a1f29ee49d5e8e
maheepisher <maheep.isher@gmail.com>
2025-04-10 21:27:59 -0400

    remove unnecessary files

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Replace __API_URL__ placeholder with the actual API_URL environment variable
-sed -i "s|__API_URL__|${API_URL}|g" /usr/share/nginx/html/*.js
-
-# Start Nginx
-nginx -g 'daemon off;'
